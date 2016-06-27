3e108de2cb8175a2fc573a31b16a00291f4dd7d2
Andrew Wilson <wilson@Andrews-MacBook-Pro.local>
2016-06-27 12:34:35 -0400

    Add a custom formatter and move CCLogger to the loggers file

--- cc_logger/__init__.py
+++ cc_logger/__init__.py
@@ -1,9 +1,12 @@
 import logging
 import logging.handlers
 import re
-from logging.config import dictConfig
+
+from logstash_formatter import LogstashFormatter
 
 from threading import Lock
+
+from .loggers import sanitize_name
 
 _log_lock = Lock()
 _log_registy = {}
@@ -17,15 +20,24 @@ logging.addLevelName(LOGSTASH, 'LOGSTASH')
 _spaces_re = re.compile(r'\s+')
 
 
-def sanitize_name(name):
-    return _spaces_re.sub('_', name.strip())
+class CCLogstashLogger(DefaultLoggerClass):
 
+    def __init__(self, name, **kwargs):
+        if not isinstance(name, basestring):
+            raise TypeError('name must be of type string, received type %s', type(name))
 
-class CCLoggerAdapter(logging.LoggerAdapter):
+        if not name.strip():
+            raise ValueError('name must be non-empty')
+
+        self.name = sanitize_name(name)
+        logging.Logger.__init__(self, self.name, **kwargs)
+
     def event(self, event_name, **extra):
         event_name = sanitize_name(event_name)
         extra.update({
             'DTM_EVENT': event_name,
+            'app_name': self.name,
+            'environment': self.environment,
         })
         self.log(LOGSTASH, 'event: %s' % event_name, extra=extra)
 
@@ -33,8 +45,10 @@ class CCLoggerAdapter(logging.LoggerAdapter):
         timer_name = sanitize_name(timer_name)
         extra.update({
             'DTM_STATS': timer_name,
+            'app_name': self.name,
             'stat_value': timer_value,
             'stat_type': 'timer',
+            'environment': self.environment,
         })
         self.log(LOGSTASH, 'timer: %s: %s' % (timer_name, timer_value), extra=extra)
 
@@ -42,7 +56,9 @@ class CCLoggerAdapter(logging.LoggerAdapter):
         counter_name = sanitize_name(counter_name)
         extra.update({
             'DTM_STATS': counter_name,
+            'app_name': self.name,
             'stat_type': 'counter',
+            'environment': self.environment,
         })
         self.log(LOGSTASH, 'counter: %s' % counter_name, extra=extra)
 
@@ -50,51 +66,43 @@ class CCLoggerAdapter(logging.LoggerAdapter):
         gauge_name = sanitize_name(gauge_name)
         extra.update({
             'DTM_STATS': gauge_name,
+            'app_name': self.name,
             'stat_value': gauge_value,
             'stat_type': 'gauge',
+            'environment': self.environment,
         })
         self.log(LOGSTASH, 'gauge: %s' % gauge_name, extra=extra)
 
 
-def create_logger(name, environment='', level=None):
-    base_config = {
-        u'version': 1,
-        u'formatters': {
-            u'logstashformatter': {
-                u'()': u'logstash_formatter.LogstashFormatter'
-            },
-        },
-        u'handlers': {
-            u'console': {
-                u'class': u'logging.StreamHandler',
-                u'level': u'DEBUG',
-                u'formatter': u'logstashformatter',
-                u'stream': u'ext://sys.stdout',
-            },
-        },
-        u'disable_existing_loggers': False,
-    }
+logging.setLoggerClass(CCLogstashLogger)
 
-    name = sanitize_name(name)
 
+def create_logger(name, filehandler_config=None, environment='', stream_config=None, level=None):
     if name in _log_registy:
         return _log_registy[name]
 
     if level is None:
         level = LOGSTASH
 
-    # Set up a new logger and add all of the handlers to it.
-    loggers = base_config.get(u'loggers', {})
-    loggers[name] = {
-        u'level': level,
-        u'handlers': [base_handler for base_handler in base_config.get(u'handlers', [])],
-    }
-    base_config[u'loggers'] = loggers
-    dictConfig(base_config)
-
     with _log_lock:
         logger = logging.getLogger(name)
-        logger.environment = environment
-        adapter = CCLoggerAdapter(logger, {'environment': environment})
-        _log_registy[name] = adapter
-        return adapter
+        logger.environment = sanitize_name(environment)
+        logger.setLevel(level)
+
+        logstash_formatter = LogstashFormatter()
+
+        if filehandler_config:
+            fh_handler = logging.handlers.RotatingFileHandler(**filehandler_config)
+            fh_handler.setLevel(level)
+            fh_handler.setFormatter(logstash_formatter)
+            logger.addHandler(fh_handler)
+
+        if stream_config:
+            stream_handler = logging.StreamHandler(stream=stream_config.get('stream'))
+            stream_handler.setLevel(stream_config.get('level', level))
+            stream_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            stream_handler.setFormatter(stream_formatter)
+            logger.addHandler(stream_handler)
+
+        _log_registy[name] = logger
+        return logger
---
+++ cc_logger/loggers.py
@@ -0,0 +1,130 @@
+import logging
+import logging.handlers
+import re
+from logging.config import dictConfig
+
+from threading import Lock
+
+_log_lock = Lock()
+_log_registy = {}
+
+
+default_level = logging.INFO
+
+_spaces_re = re.compile(r'\s+')
+
+
+def sanitize_name(name):
+    return _spaces_re.sub('_', name.strip())
+
+
+class CCLoggerAdapter(logging.LoggerAdapter):
+
+    def process(self, msg, kwargs):
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        kwargs['extra'].update(self.extra)
+
+        return msg, kwargs
+
+    def event(self, event_name, level=default_level, **kwargs):
+        event_name = sanitize_name(event_name)
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        if not isinstance(kwargs['extra'], dict):
+            raise TypeError('The extra kwarg must be a dictionary.')
+
+        kwargs['extra'].update({
+            'DTM_EVENT': event_name,
+        })
+        self.log(level, 'event: %s' % event_name, **kwargs)
+
+    def timer(self, timer_name, timer_value, level=default_level, **kwargs):
+        timer_name = sanitize_name(timer_name)
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        if not isinstance(kwargs['extra'], dict):
+            raise TypeError('The extra kwarg must be a dictionary.')
+
+        kwargs['extra'].update({
+            'DTM_STATS': timer_name,
+            'stat_value': timer_value,
+            'stat_type': 'timer',
+        })
+        self.log(level, 'timer: %s: %s' % (timer_name, timer_value), **kwargs)
+
+    def counter(self, counter_name, level=default_level, **kwargs):
+        counter_name = sanitize_name(counter_name)
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        if not isinstance(kwargs['extra'], dict):
+            raise TypeError('The extra kwarg must be a dictionary.')
+
+        kwargs['extra'].update({
+            'DTM_STATS': counter_name,
+            'stat_type': 'counter',
+        })
+        self.log(level, 'counter: %s' % counter_name, **kwargs)
+
+    def gauge(self, gauge_name, gauge_value, level=default_level, **kwargs):
+        gauge_name = sanitize_name(gauge_name)
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        if not isinstance(kwargs['extra'], dict):
+            raise TypeError('The extra kwarg must be a dictionary.')
+
+        kwargs['extra'].update({
+            'DTM_STATS': gauge_name,
+            'stat_value': gauge_value,
+            'stat_type': 'gauge',
+        })
+        self.log(level, 'gauge: %s' % gauge_name, **kwargs)
+
+
+def create_logger(name, environment='', level=None):
+    base_config = {
+        u'version': 1,
+        u'formatters': {
+            u'logstashformatter': {
+                u'()': u'cc_logger.formatters.CCFormatter'
+            },
+        },
+        u'handlers': {
+            u'console': {
+                u'class': u'logging.StreamHandler',
+                u'level': u'DEBUG',
+                u'formatter': u'logstashformatter',
+                u'stream': u'ext://sys.stdout',
+            },
+        },
+        u'disable_existing_loggers': False,
+    }
+
+    name = sanitize_name(name)
+
+    if name in _log_registy:
+        return _log_registy[name]
+
+    if level is None:
+        level = default_level
+
+    # Set up a new logger and add all of the handlers to it.
+    loggers = base_config.get(u'loggers', {})
+    loggers[name] = {
+        u'level': level,
+        u'handlers': [base_handler for base_handler in base_config.get(u'handlers', [])],
+    }
+    base_config[u'loggers'] = loggers
+    dictConfig(base_config)
+
+    with _log_lock:
+        logger = logging.getLogger(name)
+        logger.environment = environment
+        adapter = CCLoggerAdapter(logger, {'environment': environment})
+        _log_registy[name] = adapter
+        return adapter
